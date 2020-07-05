0f121b0cd5fdb20cdac7ad60833431b3a8c127d8
Marcus <mefchristiansen@gmail.com>
2020-07-05 12:46:29 -0400

    clean up backtracker comments

--- src/main/java/model/generators/RecursiveBacktracker.java
+++ src/main/java/model/generators/RecursiveBacktracker.java
@@ -1,25 +1,28 @@
 package model.generators;
 
+import controller.MazeController;
 import model.Cell;
 import model.Cell.CellVisitState;
 import model.Direction;
 import model.Maze;
 import model.MazeGeneratorWorker;
-import controller.MazeController;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Stack;
 import java.util.concurrent.CancellationException;
 
 /**
  * A SwingWorker class (extending MazeGeneratorWorker) that implements the recursive backtracker maze generation
  * algorithm. This algorithm is a DFS implementation which randomly picks an unvisited neighboring cell at each
  * iteration to visit to construct the maze.
- *
+ * <p>
  * https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker
  */
 public class RecursiveBacktracker extends MazeGeneratorWorker {
 	public RecursiveBacktracker(Maze maze, MazeController mazeController) {
-	    super(maze, mazeController);
+		super(maze, mazeController);
 	}
 
 	/**
@@ -34,56 +37,56 @@ public class RecursiveBacktracker extends MazeGeneratorWorker {
 	protected Boolean doInBackground() throws Exception {
 		Stack<Cell> searchStack = new Stack<>();
 		Cell root, current, unvisitedNeighbor;
-	    Random rand = new Random();
+		Random rand = new Random();
 
-	    int startRow = rand.nextInt(maze.numRows() - 1);
-	    int startCol = rand.nextInt(maze.numCols() - 1);
+		int startRow = rand.nextInt(maze.numRows() - 1);
+		int startCol = rand.nextInt(maze.numCols() - 1);
 
-	    root = maze.mazeCell(startRow, startCol);
+		root = maze.mazeCell(startRow, startCol);
 
-	    searchStack.push(root);
+		searchStack.push(root);
 		root.setVisitState(CellVisitState.VISITING);
 
-	    while (!searchStack.isEmpty()) {
-	    	current = searchStack.peek();
+		while (!searchStack.isEmpty()) {
+			current = searchStack.peek();
 			current.setCurrent(true);
-	        unvisitedNeighbor = unvisitedNeighbor(current);
+			unvisitedNeighbor = unvisitedNeighbor(current);
 
-	        if (unvisitedNeighbor != null) { // There is an unvisited neighboring cells to visit from the current cell.
-	            searchStack.push(unvisitedNeighbor);
+			if (unvisitedNeighbor != null) { // There is an unvisited neighboring cells to visit from the current cell.
+				searchStack.push(unvisitedNeighbor);
 				unvisitedNeighbor.setVisitState(CellVisitState.VISITING);
 
-	            removeWalls(current, unvisitedNeighbor);
-	        } else {
+				removeWalls(current, unvisitedNeighbor);
+			} else {
 	        	/*
 	        		There are no unvisited neighboring cells for the current cell. Backtrack to the cell that was
 	        		visited previously to find an unvisited neighboring cell from there.
 	        	 */
 				current.setVisitState(CellVisitState.VISITED);
-	            searchStack.pop();
-	        }
+				searchStack.pop();
+			}
 
-	        publish(maze); // Publish the current maze state to be repainted on the event dispatch thread.
+			publish(maze); // Publish the current maze state to be repainted on the event dispatch thread.
 
-            Thread.sleep(mazeController.getAnimationSpeed());
+			Thread.sleep(mazeController.getAnimationSpeed());
 
 			current.setCurrent(false);
 		}
 
 		maze.voidVisits();
-	    return true;
+		return true;
 	}
 
 	/**
 	 * Override of the SwingWorker process function for the recursive backtracker SwingWorker, which repaints the maze
 	 * at every iteration of maze generation asynchronously on the event dispatch thread.
 	 */
-    @Override
-    protected void process(List<Maze> chunks) {
-        for (Maze maze : chunks) {
-            mazeController.repaintMaze(maze);
-        }
-    }
+	@Override
+	protected void process(List<Maze> chunks) {
+		for (Maze maze : chunks) {
+			mazeController.repaintMaze(maze);
+		}
+	}
 
 	/**
 	 * Override of the SwingWorker done function (run after the thread is completed) for the recursive backtracker
@@ -92,22 +95,22 @@ public class RecursiveBacktracker extends MazeGeneratorWorker {
 	 * reset function in the controller, and any clean-up will be handled there. For other exceptions, these will not
 	 * have been triggered by the maze reset function, so that will trigger the maze reset function to clean up.
 	 */
-    @Override
-    protected void done() {
-    	Boolean status;
+	@Override
+	protected void done() {
+		Boolean status;
 
-    	try {
-    		status = get();
-    		if (status) {
-    			mazeController.generateMazeSuccess();
-    		} else {
-    			mazeController.reset();
-    		}
-    	} catch (CancellationException ignore) {
-    	} catch (Exception e) {
-    		mazeController.reset();
-    	}
-    }
+		try {
+			status = get();
+			if (status) {
+				mazeController.generateMazeSuccess();
+			} else {
+				mazeController.reset();
+			}
+		} catch (CancellationException ignore) {
+		} catch (Exception e) {
+			mazeController.reset();
+		}
+	}
 
 	/**
 	 * Iterates through all neighbours of the currently visited cell (up, down left, right), and returns a randomly
@@ -116,36 +119,36 @@ public class RecursiveBacktracker extends MazeGeneratorWorker {
 	 * @param current The current cell being visited by the recursive backtracker
 	 * @return A randomly picked valid (i.e. in bounds) neighboring cell that has not already been visited
 	 */
-    private Cell unvisitedNeighbor(Cell current) {
-    	Cell unvisitedNeighbor;
-    	Random rand = new Random();
-    	List<Cell> unvisitedNeighbors = new ArrayList<>();
-    	int currRow = current.row();
-    	int currCol = current.col();
-    	int newRow, newCol;
+	private Cell unvisitedNeighbor(Cell current) {
+		Cell unvisitedNeighbor;
+		Random rand = new Random();
+		List<Cell> unvisitedNeighbors = new ArrayList<>();
+		int currRow = current.row();
+		int currCol = current.col();
+		int newRow, newCol;
 
-	    for (Direction dir : Direction.values()) {
-	        newRow = currRow + dir.dy;
-	        newCol = currCol + dir.dx;
+		for (Direction dir : Direction.values()) {
+			newRow = currRow + dir.dy;
+			newCol = currCol + dir.dx;
 
 			// Check that the cell is in the bounds of the maze
 			if (!maze.inBounds(newRow, newCol)) {
 				continue;
 			}
 
-	        unvisitedNeighbor = maze.mazeCell(newRow, newCol);
+			unvisitedNeighbor = maze.mazeCell(newRow, newCol);
 
-	        // Check that the cell is in the bounds of the maze, and that it hasn't already been visited
-	        if (unvisitedNeighbor.unvisited()) {
-	            unvisitedNeighbors.add(maze.mazeCell(newRow, newCol));
-	        }
-	    }
+			// Check that the cell is in the bounds of the maze, and that it hasn't already been visited
+			if (unvisitedNeighbor.unvisited()) {
+				unvisitedNeighbors.add(maze.mazeCell(newRow, newCol));
+			}
+		}
 
-	    if (unvisitedNeighbors.size() == 0) {
-	        return null;
-	    }
+		if (unvisitedNeighbors.size() == 0) {
+			return null;
+		}
 
-	    return unvisitedNeighbors.get(rand.nextInt(unvisitedNeighbors.size()));
+		return unvisitedNeighbors.get(rand.nextInt(unvisitedNeighbors.size()));
 	}
 
 	/**
@@ -154,12 +157,12 @@ public class RecursiveBacktracker extends MazeGeneratorWorker {
 	 * the current cell, then the current cell needs to remove its right wall, whereas the neighboring cell needs to
 	 * remove it left wall.
 	 *
-	 * @param current A cell
+	 * @param current   A cell
 	 * @param neighbour A neighboring cell
 	 */
 	private void removeWalls(Cell current, Cell neighbour) {
-	    Direction direction = current.directionToCell(neighbour);
-	    current.removeWall(direction);
-	    neighbour.removeWall(direction.oppositeDirection());
+		Direction direction = current.directionToCell(neighbour);
+		current.removeWall(direction);
+		neighbour.removeWall(direction.oppositeDirection());
 	}
 }
